d4c1da6e974ed82774ef7f59e4946c7b7d1019de
ahmed777ismail <ahmed1kaballo@gmail.com>
2024-01-25 13:11:35 +0200

    Add Images & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,50 +12,118 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      home: SimpleProject(),
+    return const MaterialApp(
+      home: FacebookApp(),
     );
   }
 }
 
-class SimpleProject extends StatelessWidget {
-  const SimpleProject({super.key});
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 10,
-        title: Text("facebook"),
-        centerTitle: false,
+        title: Text("facebook",
+            style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 27,
+                fontWeight: FontWeight.w700)),
+        centerTitle: true,
         leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.menu,
-              size: 33,
-            )),
+          icon: Icon(
+            Icons.menu,
+            color: Colors.blueAccent,
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
         actions: [
           IconButton(
-              onPressed: () {},
               icon: Icon(
                 Icons.message,
-                size: 25,
-              )),
+                color: Colors.blueAccent,
+                size: 30,
+              ),
+              onPressed: () {}),
           IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                size: 25,
-              )),
+            icon: Icon(
+              Icons.search,
+              color: Colors.blueAccent,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
         ],
+        backgroundColor: Colors.white,
+        elevation: 20,
       ),
-      body: Center(
-        child: Text(
-          "Hello,Ahmed 👋",
-          style: TextStyle(fontSize: 40),
-        ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 33),
+                child: Text(
+                  "Scania",
+                  style: TextStyle(
+                      color: Colors.blue[800],
+                      fontSize: 99,
+                      fontFamily: "myfont",
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 44),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(13),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.blue, width: 1)),
+                      child: SvgPicture.asset(
+                        "assets/img/face.svg",
+                        color: Colors.blue[700],
+                        height: 44,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 22,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(13),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.blue, width: 1)),
+                      child: SvgPicture.asset(
+                        "assets/img/insta.svg",
+                        color: Colors.blue[700],
+                        height: 44,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 22,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(13),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.blue, width: 1)),
+                      child: SvgPicture.asset(
+                        "assets/img/twitter.svg",
+                        color: Colors.blue[700],
+                        height: 44,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
       ),
     );
   }
